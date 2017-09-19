fa4ab01a1140f564a6ea43ebeb2f6bb271769c03
Erick Perez <NBA_EAP@hotmail.com>
2017-09-19 14:30:49 -0600

    Finish form for add page

--- midtermProject/add.php
+++ midtermProject/add.php
@@ -18,25 +18,25 @@
 
 <main class="mainContent">
 	
-	<h1>Add new employee</h1>
+	<h1>Add a new employee</h1>
 	
 	<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data" class="mainForm">
 		<fieldset>
 			<legend>Employee Information</legend>
-			<input type="text" name="first" placeholder="First Name" class="userInput" pattern="[a-zA-Z .,-]{2, 99}" required>
-			<input type="text" name="last" placeholder="Last Name" class="userInput" pattern="[a-zA-Z .,-]{2, 99}" required>
-			<input type="text" name="address" placeholder="123 some st." class="userInput" pattern="[a-zA-Z .,-]{2, 99}" required>
-			<input type="tel" name="phone" placeholder="123-555-5555" class="userInput" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
-			<input type="email" name="email" placeholder="name@you.com" class="userInput" required>
+			<input type="text" name="first" placeholder="First Name" class="userInput" pattern="[a-zA-Z .,-]{2, 99}" title="Enter a first name" required>
+			<input type="text" name="last" placeholder="Last Name" class="userInput" pattern="[a-zA-Z .,-]{2, 99}" title="Enter a last name" required>
+			<input type="text" name="address" placeholder="Address" class="userInput" pattern="[a-zA-Z .,-]{2, 99}" title="Enter a valid address" required>
+			<input type="tel" name="phone" placeholder="Phone ex.123-555-5555" class="userInput" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" title="Enter a valid phone number" required>
+			<input type="email" name="email" placeholder="Email" class="userInput" title="Enter a valid email address" required>
 		</fieldset>
 		
 		<fieldset>
 			<legend>Skills &amp; Expertise</legend>
 			<span>Bio:</span>
-			<textarea name="bio" class="userInput"></textarea>
+			<textarea name="bio" class="bioText" placeholder="Enter bio description here..."></textarea>
 			
 			<span>Area of Expertise</span>
-			<select name="experise" class="userInput">
+			<select name="expertise" class="expertiseSelect">
 				 <option>Physics</option>
 				 <option>Geography</option>
 				 <option>Anthropology</option>
@@ -48,11 +48,13 @@
 		<fieldset>
 			<legend>Upload employee picture</legend>
 			<label>
-			<input name="photo" type="file"><br>
-			<span>File must be saved as a .jpg file.</span>
+			<input name="photo" type="file" class="uploadButton"><br>
+			<span>File must be saved as a .jpg file.</span><br>
 			<span>Please crop to 300px x 200px, before uploading.</span>
 			</label>
 		</fieldset>
+		
+		<input name="submit" class="submitButton" value="Add Employee" type="submit">
 	</form>
 	
 </main>
