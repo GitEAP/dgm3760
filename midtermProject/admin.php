9989e29d47e4080937b17d9c6ed8ad2751113a0e
Erick Perez <NBA_EAP@hotmail.com>
2017-09-20 17:28:18 -0600

    Add admin page with styles

--- midtermProject/admin.php
+++ midtermProject/admin.php
@@ -1,3 +1,9 @@
+<?php require_once('connectvars.php'); 
+
+$dbconnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Connection failed');
+$query = "SELECT * FROM employee_directory";
+$result = mysqli_query($dbconnection, $query);
+?>
 <?php include_once('htmlHead.php'); ?>
 <body>
 
@@ -9,21 +15,53 @@
 	</ul>
 </nav>
 
-<div class="container">
+<div id="top" class="container">
 
 <header class="headerWrapper">
 	<i class="fa fa-bars openBtn"></i>
 	<h1>Advanced Research Studies<i class="fa fa-flask"></i></h1>
 </header>
 
-	<div class="banner"></div>
+<main class="mainContent clearfix">
+	
 
-<main class="mainContent">
+<?php 
+	echo '<h1>Employees</h1>';
 	
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quia, aliquam unde laudantium ex animi alias et natus, neque aperiam perspiciatis ullam. Molestias consequatur culpa pariatur quos temporibus optio dolore?</p>
+	echo '<p>Hiring an employee?</p><a href="add.php" class="linkButton"><i class="fa fa-user-plus"></i> Add Employee</a>';
 	
-	<a href="add.php">Add employee</a>
+		while ($row = mysqli_fetch_array($result)) {
 	
+	//Verify the photo exits
+	if (file_exists('images/' . $row['photo']) && $row['photo'] <> '') {
+		$photoPath = 'images/' . $row['photo'];
+	} else {
+		$photoPath = 'images/noimagefound.jpg';
+	}
+
+	//display all details of employees
+		echo '<div class="detailContainer clearfix">';
 	
+		echo '<figure class="detailImg">';
+			echo '<img src="' . $photoPath . '" alt="Picture of employee">';
+		echo '</figure>';	
+	
+		echo '<div class="detailContent">';
+		echo '<h3>' . $row['first'] . ' ' .$row['last'] . '</h3>';
+		echo '<p>Address: ' . $row['address'] . '</p>';
+		echo '<p>Phone: ' . $row['phone'] . '</p>';
+		echo '<p>Area: ' . $row['area'] . '</p>';
+		echo '<p>Bio:<br>' . $row['bio'] . '</p>';
+
+		echo '<hr>';	
+	
+		echo '<a href="#" class="linkButton"><i class="fa fa-edit"></i>Update</a>';
+		echo '<a href="#" class="deleteButton"><i class="fa fa-trash-o"></i> Delete</a>';
+		echo '</div>';//end of content
+
+		echo '</div>';//end of container
+		}//end of while loop
+?>
+	<a href="#top" class="linkButton"><i class="fa fa-hand-o-up"></i> Back to top</a>
 </main>
 <?php include_once('footer.php'); ?>
