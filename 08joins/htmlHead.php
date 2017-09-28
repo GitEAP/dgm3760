81c5e8544718e1b85b289a77ecec08d1e889d9c0
Erick Perez <NBA_EAP@hotmail.com>
2017-09-28 10:39:03 -0600

    Setup new project

---
+++ 08joins/htmlHead.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<!-- TELLS PHONES NOT TO LIE ABOUT THEIR WIDTH & stops the font from enlarging whena phone is turned sideways-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Advanced Research Studies</title>
+<link href="styles/main.css" type="text/css" rel="stylesheet">
+<!--Google Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet"> 
+<!-- Icons -->
+<script src="https://use.fontawesome.com/a63492021a.js"></script>
+<!--scripts-->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="menu.js" type="text/javascript"></script>
+<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+</head>
---
+++ 08joins/index.php
@@ -0,0 +1,33 @@
+<?php include_once('htmlHead.php'); ?>
+<body>
+
+<nav class="mainNav" id="mainSideNav">
+	<ul>
+		<li><a href="javascript:void(0)" class="fa fa-close closeBtn"></a></li>
+		<li class="active"><a href="index.php">Employees</a></li>
+		<li><a href="admin.php">Admin</a></li>
+	</ul>
+</nav>
+
+
+
+
+<div class="container">
+
+<header class="headerWrapper">
+	<i class="fa fa-bars openBtn"></i>
+	<h1>Advanced Research Studies<i class="fa fa-flask"></i></h1>
+</header>
+
+	<div class="banner"></div>
+
+<main class="mainContent clearfix">
+	
+	<h1>Research Team</h1>
+	<div class="row clearfix">
+
+
+	</div><!--end of row-->
+	
+</main>
+<?php include_once('footer.php'); ?>
