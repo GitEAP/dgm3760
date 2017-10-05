7c854caf9081ce00659f6e10b22b224334ee93e5
Erick Perez <NBA_EAP@hotmail.com>
2017-10-05 12:27:17 -0600

    Add new project

---
+++ 09toolbox/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<p>&copy; 2017 &bull; Erick Perez &bull; Built for DGM 3760 Web Languages 2</p>
+</footer>
+</body>
+</html>
---
+++ 09toolbox/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once('connectvars.php');
+$dbconnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Connection failed');
+
+$query = "SELECT * FROM movie_user INNER JOIN movie_fav ON (movie_user.favorite = movie_fav.fav_id) $queryWhere ORDER BY last";
+$result = mysqli_query($dbconnection, $query) or die('inner join failed');
+
+?>
+<?php include_once('htmlHead.php'); ?>
+<body>
+<?php include_once('header.php'); ?>
+
+<main class="mainContent clearfix">
+	
+	
+	
+	
+	
+	
+	
+	
+
+</main>
+<?php mysqli_close($dbconnection); ?>
+<?php include_once('footer.php'); ?>
