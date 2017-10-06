e819d67787005887eedef93c6692bf9d07aa9e57
Erick Perez <NBA_EAP@hotmail.com>
2017-10-06 11:28:51 -0600

    Add new project

---
+++ finalProject/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<!-- TELLS PHONES NOT TO LIE ABOUT THEIR WIDTH & stops the font from enlarging whena phone is turned sideways-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>OK.com | Family Media Guide</title>
+<link href="styles/styles.css" type="text/css" rel="stylesheet">
+<!--Google Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet"> 
+<!-- Icons -->
+<script src="https://use.fontawesome.com/a63492021a.js"></script>
+<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+</head>
+<body>
+
+
+
+
+
+
+</body>
+</html>
