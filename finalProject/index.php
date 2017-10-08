f35677c960968d3f085b35466beeea3853bbe5c5
Erick Perez <NBA_EAP@hotmail.com>
2017-10-08 17:04:06 -0600

    Add main layout styles

--- finalProject/index.php
+++ finalProject/index.php
@@ -10,6 +10,9 @@
 <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet"> 
 <!-- Icons -->
 <script src="https://use.fontawesome.com/a63492021a.js"></script>
+<!--scripts-->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="menu.js" type="text/javascript"></script>
 <!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
@@ -17,9 +20,144 @@
 </head>
 <body>
 
+<header class="clearfix">
+	<div class="headerColorBar clearfix"></div>
+	
+	<div class="headerContainer clearfix">
+	
+	<div class="headerLeft">
+		<span onclick="openNav()" class="openbtn"><i class="fa fa-bars"></i></span>
+		
+		<img src="assets/okLogo.png" alt="logo">
+		<h1>Your family's media guide</h1>
+	</div>
+	
+	<div class="headerRight">
+		<div class="socialIcons">
+			<ul>
+				<li><a href="#"><i class="fa fa-facebook-official"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+				<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus-official"></i></a></li>
+			</ul>
+		</div><!--//end of social icons-->
+		
+		<form action="" method="POST" enctype="multipart/form-data" class="searchBar">
+			<input type="text" class="searchField" name="search" placeholder="Search movie titles...">
+			<input type="submit" class="searchButton" name="submit" value="Search">
+		</form>
+		
+		
+	</div><!--//end of header right-->
+	</div><!--//end of header container-->
+</header>
 
 
 
+<main class="mainContent clearfix">
+
+<div class="contentContainer clearfix">
+
+
+<aside class="contentLeft">
+
+<nav class="mainNav">
+	<ul>
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><i class="fa fa-times-circle"></i></a>
+		<li class="active"><a href="#"><i class="fa fa-home"></i>  Home</a></li>
+		<li><a href="#"><i class="fa fa-home"></i>  Movies</a></li>
+		<li><a href="#"><i class="fa fa-home"></i>  Reviews</a></li>
+		<li><a href="#"><i class="fa fa-home"></i>  Users</a></li>
+	</ul>
+</nav>
+
+
+
+<div class="sideBox">
+	<img src="assets/dont-miss-out.png" alt="tape icon">
+	<p>Follow trusted reviewers</p>
+	<hr>
+	<p>Get a personalized activity feed</p>
+	<hr>
+	<p>Share your own ratings and reviews</p>
+	<button class="button1">Join OK.com</button>
+	<button class="button2">Sign In</button>
+</div>
+
+
+
+<div class="socialIconsSide">
+	<ul>
+		<li><a href="#"><i class="fa fa-facebook-official"></i></a></li>
+		<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+		<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+		<li><a href="#"><i class="fa fa-google-plus-official"></i></a></li>
+	</ul>
+</div><!--//end of social icons-->
+
+
+<div class="sideBox2">
+<p>© 2017 Ok.com  | <a href="#">Our Mission</a> | <a href="#">For Publishers</a> | <a href="#">Contact Us</a> | <a href="#">Terms of Service</a> | <a href="#">Privacy Policy</a></p>
+</div><!-- end of sideBox 2-->
+</aside>
+
+
+<section class="contentRight">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+
+<p>Lorem ipsum dolor sit amet, consloremectetur adipisicing elit. Vitae officiis vel, et sit explicabo optio quasi nihil harum voluptates consequuntur, animi voluptas. Inventore natus sequi quos sed rerum, laborum maiores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolore cum nam suscipit sed praesentium mollitia inventore temporibus repudiandae fugiat ut minus, natus consequatur soluta molestias odio vel facilis nulla. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id quos dolore, neque voluptatem voluptatibus quod voluptates voluptatum repudiandae illo odio, reprehenderit vero, nobis sit minima ratione enim at aspernatur tempora!</p>
+
+
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique molestias hic dolores veritatis fuga rem inventore quis, eos cupiditate asperiores quibusdam ipsa voluptatem nihil velit iure aut, ad adipisci voluptatibus!</p>
+					
+</section><!--//end of content right			-->
+	
+	
+	
+	
+	</div><!--//end of content container-->
+	</main>
 
 
 </body>
